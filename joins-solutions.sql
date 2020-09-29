e81c252d8588f8ef9c2b0f273b950b7638c3222b
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-29 14:16:22 -0500

    completed base mode

--- joins-solutions.sql
+++ joins-solutions.sql
@@ -28,13 +28,32 @@ WHERE "products"."id" = 6;
 
 -- 5. Get the number of orders for each customer. 
 --    NOTE: It is OK if those without orders are not included in results.
-
+SELECT "customers"."last_name", COUNT("orders"."id")
+FROM "orders"
+JOIN "addresses"
+ON "orders"."address_id" = "addresses"."id"
+JOIN "customers"
+ON "addresses"."customer_id" = "customers"."id"
+GROUP BY "customers"."last_name";
 
 -- 6. How many customers do we have?
-
+SELECT COUNT(*) FROM "customers";
 
 -- 7. How many products do we carry?
-
+SELECT COUNT(*) FROM "products";
 
 -- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT SUM("warehouse_product"."on_hand") FROM "warehouse_product"
+WHERE "product_id" = 6;
+
+--STRETCH GOALS
+-- 9. How much was the total cost for each order?
+
+
+-- 10. How much has each customer spent in total?
+
+
+-- 11. How much has each customer spent in total? 
+--     Customers who have spent $0 should still show up in the table. 
+--     It should say 0, not NULL (research coalesce).
 
